b3ea74cd5169b68cbed43e26b7a6d3a17b0c6525
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2023-04-21 22:15:36 -0300

    feat: config firebase

---
+++ config/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDef8m_v5BJ6lrz_bRa0WoRdewV97Z_ZpE',
+  authDomain: 'chatapp-a5120.firebaseapp.com',
+  projectId: 'chatapp-a5120',
+  storageBucket: 'chatapp-a5120.appspot.com',
+  messagingSenderId: '752071532523',
+  appId: '1:752071532523:web:a7efb1126fff66c07d43ae'
+};
+
+const app = initializeApp(firebaseConfig);
